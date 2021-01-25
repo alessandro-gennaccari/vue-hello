5ad30398c2d6402eb85aa876f230453f24b7fbf6
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-01-25 14:32:42 +0100

    all-done

--- js/script.js
+++ js/script.js
@@ -2,3 +2,22 @@
 Bonus: Aggiungere un’immagine presa anch’essa da un data assieme al valore alt dell'attributo.
 Poi aggiungete all'immagine una classe sempre presa dal data. */
 
+var ready = new Vue({
+    el: '#hello',
+    data: {
+        message: 'Primo approccio a Vue.js',
+        src: 'https://vuejs.org/images/logo.png',
+        alt: 'Logo Vue',
+        textButton: 'Inverti colori',
+        logoFormat: 'logo',
+        color: 'white',
+        bgColor: 'bgc-black'
+    },
+    methods: {
+        inverter() {
+            (this.color == 'white') 
+            ? (this.color = 'black',  this.bgColor = 'bgc-white')
+            : (this.color = 'white', this.bgColor = 'bgc-black');
+        }
+    }
+});
